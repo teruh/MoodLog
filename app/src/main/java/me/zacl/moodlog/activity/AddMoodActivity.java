4c8882d3b51d533feda1e445bdcb8c17519084e8
teruh <zachclarkbyte@gmail.com>
2020-09-20 19:14:54 -0400

    Add basic UI widgets to AddMoodActivity

--- app/src/main/java/me/zacl/moodlog/activity/AddMoodActivity.java
+++ app/src/main/java/me/zacl/moodlog/activity/AddMoodActivity.java
@@ -1,14 +1,25 @@
 package me.zacl.moodlog.activity;
 
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import me.zacl.moodlog.R;
 
 public class AddMoodActivity extends AppCompatActivity {
 
+   private SeekBar  rateMoodSeekbar;
+   private EditText notesEditText;
+   private Button   saveButton;
+
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_add_mood);
+
+      rateMoodSeekbar = findViewById(R.id.moodRatingSeekbar);
+      notesEditText = findViewById(R.id.notesEditText);
+      saveButton = findViewById(R.id.saveButton);
    }
 }
